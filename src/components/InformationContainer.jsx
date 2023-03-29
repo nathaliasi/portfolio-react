e1171d3960eb0e2ef5ba149dc293cf7495e86482
Nathália Sardou <nathaliatrab@outlook.com>
2023-03-29 02:35:43 -0300

    consegui colocar responsivo no sass

--- src/components/InformationContainer.jsx
+++ src/components/InformationContainer.jsx
@@ -11,7 +11,8 @@ const InformationContainer = () => {
             <AiFillPhone id='phone-icon' />
             <div>
                 <h3>Celular</h3>
-                <p>(22)99898-2554</p>
+                <a href='https://api.whatsapp.com/send?1=pt_BR&phone=5522998982554' target={'_blank'}>(22)99898-2554</a>
+                
             </div>
         </div>
         <div className='info-card'>
